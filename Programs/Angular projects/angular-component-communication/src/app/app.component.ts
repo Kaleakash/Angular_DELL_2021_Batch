00b6fa0aa417fb4aceed8f02fb2a3f1828f9f365
kaleakash <akash300383@gmail.com>
2021-04-20 11:01:57 +0530

    child to parent using @viewchild

--- Programs/Angular projects/angular-component-communication/src/app/app.component.ts
+++ Programs/Angular projects/angular-component-communication/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Child2Component } from './child2/child2.component';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
  parentName?:string;
  cAge?:number;
+childDesg?:string;
+ @ViewChild(Child2Component)
+ child2?:Child2Component;       //DI for child2 
+
  passName(parentName:string){
    this.parentName= parentName;
+ }
+ constructor(){
+   
+ }
+
+ callChild2() {
+  this.child2?.dis();
+  this.childDesg=this.child2?.desg;
  }
 }
