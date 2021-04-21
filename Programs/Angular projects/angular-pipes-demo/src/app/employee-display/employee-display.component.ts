2f1befb352ab1cf33fcee8e4edbd53cb91a44f4b
kaleakash <akash300383@gmail.com>
2021-04-21 11:04:42 +0530

    sorting

--- Programs/Angular projects/angular-pipes-demo/src/app/employee-display/employee-display.component.ts
+++ Programs/Angular projects/angular-pipes-demo/src/app/employee-display/employee-display.component.ts
@@ -9,11 +9,24 @@ import { Employee } from '../model.employee';
 })
 export class EmployeeDisplayComponent implements OnInit {
   employees?:Array<Employee>;
+  dd=new Date();
   constructor(public employeeService:EmployeeService) { } //DI
 
   ngOnInit(): void {
     this.employeeService.retrieveEmployeeDetails().
     subscribe(data=>this.employees=data);
+  }
+
+  sortByName() {
+  this.employees?.sort((emp1,emp2)=>{
+    if(emp1.name>emp2.name){
+      return 1;
+    }else if(emp1.name<emp2.name){
+      return -1
+    }else {
+    return 0;
+    }
+  })
   }
 
 }
