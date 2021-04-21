d15c528350989aead35c4de11b482891578e4fd3
kaleakash <akash300383@gmail.com>
2021-04-21 11:25:58 +0530

    custom pipe

--- Programs/Angular projects/angular-pipes-demo/src/app/pipe.genderDetails.ts
+++ Programs/Angular projects/angular-pipes-demo/src/app/pipe.genderDetails.ts
@@ -4,8 +4,9 @@ import { Pipe } from "@angular/core";
     name:"GenderPipe"
 })
 export class GenderDetailsPipe implements Transformer{
-        transform(data:any[],genderDetails?:any):any{
+        transform(data:any[],genderDetails?:any,info?:string):any{
         console.log(data);
+        console.log(info)
           if(genderDetails=="male"){
                 return "Mr. "+data;
           }else {
